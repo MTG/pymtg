726b8254783547cfad82727ddb3501ea6cbe34cf
ffont <frederic.font@gmail.com>
2017-03-07 14:40:54 +0100

    Add basic package structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='pymtg',
+      version='0.1',
+      description='Python research utils that some of us use at the MTG and eventually everyone will use :)',
+      url='https://github.com/MTG/pymtg',
+      author='Music Technology Group',
+      author_email='mtg-info@upf.edu',
+      license='MIT',
+      packages=['pymtg'])
