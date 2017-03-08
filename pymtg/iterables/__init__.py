7890516d2414f11810206aa38cad97d04538730f
ffont <frederic.font@gmail.com>
2017-03-08 13:42:37 +0100

    Update pymtg.iterables.chunks docstring and rebuild docs

--- pymtg/iterables/__init__.py
+++ pymtg/iterables/__init__.py
@@ -1,6 +1,9 @@
 def chunks(l, n):
-    """
-    Yield successive ``n``-sized chunks from ``l``.
+    """Yield successive ``n``-sized chunks from ``l``.
+
+    Examples:
+        >>> list(chunks([1, 2, 3, 4, 5], 2))
+        [[1, 2], [3, 4], [5]]
     """
     for i in range(0, len(l), n):
         yield l[i:i+n]
