a5e9e3988788018c9327f3412ef99a85014418dc
ffont <frederic.font@gmail.com>
2018-02-13 10:19:46 +0100

    Add linear_approximation new function to signal module

--- pymtg/signal/__init__.py
+++ pymtg/signal/__init__.py
@@ -21,7 +21,7 @@ def smooth(x, window_len=11, window='hanning', preserve_length=True):
             as the length of the input signal (default=True).
 
     Returns:
-        (array): Tthe smoothed signal
+        (array): The smoothed signal
         
     Examples:
         >>> smooth([0, 1, 0, 1, 0, 1], 4)
@@ -47,3 +47,39 @@ def smooth(x, window_len=11, window='hanning', preserve_length=True):
     if preserve_length:
         return y[(window_len//2-1):-(window_len//2)][:x.size]
     return y
+
+
+def linear_approximation(x, include_coeffs=False):
+    """Compute the least squares polynomial fit of first degree of x (linear approximation).
+
+    This function returns the linear approximation as a signal of the same length of x.
+    If requested, the function can also return the linear approximation coefficients as
+    returned by Numpy's 'polyfit' function. For more details in the method used for the linear
+    approximation, see https://docs.scipy.org/doc/numpy/reference/generated/numpy.polyfit.html.
+
+    Args:
+        x (array): The input signal 
+        include_coeffs (bool): Whether to return the computed linear approximation coefficients 
+            along with the approximated signal (default=False).
+
+    Returns:
+        (array): The linear approximation of then input signal
+        
+    Examples:
+        >>> linear_approximation([1, 1, 1])
+        array([ 1.,  1.,  1.])
+        >>> linear_approximation([0, 1, 2, 3, 4, 5])
+        array([ 0.,  1.,  2.,  3.,  4.,  5.])
+        >>> linear_approximation([1, 2, 4, 8, 16])
+        array([ -1. ,   2.6,   6.2,   9.8,  13.4])
+        >>> linear_approximation([1, 2, 4, 8, 16], include_coeffs=True)
+        (array([ -1. ,   2.6,   6.2,   9.8,  13.4]), (3.6000000000000001, -0.99999999999999778))
+    """
+    a, b = np.polyfit(range(0, len(x)), x, 1)
+    x_fit = np.array([a*i + b for i in range(0, len(x))])
+    if not include_coeffs:
+        return x_fit
+    else:
+        return x_fit, (a, b)
+
+
