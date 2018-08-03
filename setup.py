ec0ea6b4e808582e07fd91a536dd7ffcf25ed147
ffont <frederic.font@upf.edu>
2018-08-03 16:00:39 +0200

    Update version to 0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pymtg',
-      version='0.1',
+      version='0.2',
       description='Python research utils that some of us use at the MTG and eventually everyone will use :)',
       url='https://github.com/MTG/pymtg',
       author='Music Technology Group',
