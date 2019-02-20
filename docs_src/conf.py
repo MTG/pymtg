ec13fd62bd4098dcef0e4f47a71b53836c2129ac
Alastair Porter <alastair.porter@upf.edu>
2019-02-20 16:48:57 +0100

    Build docs

--- docs_src/conf.py
+++ docs_src/conf.py
@@ -38,7 +38,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'pymtg'
-copyright = u'2017, Music Technology Group, UPF'
+copyright = u'2019, Music Technology Group, UPF'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
